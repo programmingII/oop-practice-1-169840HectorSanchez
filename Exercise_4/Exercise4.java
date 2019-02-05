00717cff401c73cc1c4c7d5739b978f4065d5883
169840 Hector Sanchez <46726559+169840HectorSanchez@users.noreply.github.com>
2019-02-05 13:41:17 -0700

    Add files via upload

---
+++ Exercise_4/Exercise4.java
@@ -0,0 +1,18 @@
+/*Hector_Gerardo_Sanchez_Quiroga
+* 13:38 
+* 05/02/19
+* Write a Java program to print the result of the following operations. 
+*/
+public class Exercise4{
+
+	public static void main(String args[]){
+
+		
+	//Se crearon 3 diferentes variables, en las que a sdiv, se le asignara el valor de a/b
+	System.out.println(-5 + 8 * 6);
+	System.out.println((55+9) % 9);
+	System.out.println(20 + - 3 * 5 / 8);
+	System.out.println(5 + 15 / 3 * 2 - 8 % 3 );
+
+}
+}
