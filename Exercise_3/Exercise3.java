1d1663a0c3d1b8e30bce99ee3be8f93fb1d96fdc
169840 Hector Sanchez <46726559+169840HectorSanchez@users.noreply.github.com>
2019-02-05 13:35:08 -0700

    Add files via upload

---
+++ Exercise_3/Exercise3.java
@@ -0,0 +1,23 @@
+/*Hector_Gerardo_Sanchez_Quiroga
+* 13:24 
+* 05/02/19
+* Write a Java program to divide two numbers and print on the screen.
+*/
+import java.util.Scanner;
+// Lectura de datos
+public class Exercise3{
+
+	public static void main(String args[]){
+	Scanner input = new Scanner(System.in);
+	int a,b,sdiv;
+		
+	//Se crearon 3 diferentes variables, en las que a sdiv, se le asignara el valor de a/b
+	System.out.println("Ingrese el primer valor: ");
+	a = input.nextInt();
+	System.out.println("Ingrese el segundo valor: ");
+	b= input.nextInt();
+	sdiv=a/b;
+	System.out.println(sdiv);
+
+}
+}
