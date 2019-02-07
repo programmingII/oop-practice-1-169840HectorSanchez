7a794083c44b6b62950cf6ee02e296e5b00ecf82
169840 Hector Sanchez <46726559+169840HectorSanchez@users.noreply.github.com>
2019-02-07 13:36:29 -0700

    Add files via upload

---
+++ Exercise_10/Exercise10.java
@@ -0,0 +1,18 @@
+/* Nombre: Hector_Gerardo_Sanchez_Quiroga_169840
+*  Dia: 02/07/19
+*  Hora: 13:20
+*@WTD: Write a Java program to compute a specified formula
+*/
+public class Exercise10{
+// A class describes its behaviour through its methods, 
+//where a method is a place where the logic of a class is stored.
+
+// public keyword, it means this class is accessible 
+//and visible to all the classes in all the packages in Java.
+
+	public static void main(String args[]){
+
+//
+		System.out.println(4.0*(1-(1.0/3)+(1.0/5)-(1.0/7)+(1.0/9)-(1.0/11)));
+}
+}
