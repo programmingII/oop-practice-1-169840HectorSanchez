08a9f310bb4badcc6b35649f0f9315528d140135
169840 Hector Sanchez <46726559+169840HectorSanchez@users.noreply.github.com>
2019-02-07 13:04:49 -0700

    Add files via upload

---
+++ Exercise_8/Exercise8.java
@@ -0,0 +1,16 @@
+/*  @autor: Hector_Sanchez_169840
+*  @date: 02/07/19
+* @timeStart: 12:46
+* @WTD: Write a Java program that takes a number as input and prints its multiplication table upto 10.
+*/
+
+public class Exercise8{
+	public static void main(String args[]){
+	
+	
+		System.out.println("   J    a   v     v  a ");
+		System.out.println("   J   a a   v   v  a a ");
+		System.out.println("J  J  aaaaa   V V  aaaaa   ");
+		System.out.println(" JJ  a     a   V  a     a");
+}
+}
