99f13e05d9eaaee7941f890376cbc0d8283616a4
169840 Hector Sanchez <46726559+169840HectorSanchez@users.noreply.github.com>
2019-02-07 13:13:53 -0700

    Update Exercise7.java

--- Exercise_7/Exercise7.java
+++ Exercise_7/Exercise7.java
@@ -1,6 +1,6 @@
-/*  @autor: Hector_Sanchez_169840
-*  @date: 02/07/19
-* @timeStart: 12:46
+/*  Nombre: Hector_Sanchez_169840
+*  Dia: 02/07/19
+* Hora: 12:46
 * @WTD: Write a Java program that takes a number as input and prints its multiplication table upto 10.
 */
 import java.util.Scanner;
@@ -14,7 +14,7 @@ public class Exercise7{
 		System.out.println("Ingresa el valor: ");
 		a= Sr.nextInt();
 //for: Utilizamos el for para realizar una iterracion para mostrar toda la tabla del 10
-//i sera el contador del for, y se utilizara el i++ para decir que se sumara i=i+1 por cada iteración
+//i sera el contador del for, y se utilizara el i++ para decir que se sumara i=i+1 por cada iteraciÃ³n
 		for(int i=1;i<11;i++){
 			System.out.println(a+" X "+i+" = "+(a*i));
 }
