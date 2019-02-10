334db07816a70336ae015b8239ed1159983e1c0a
169840 Hector Sanchez <46726559+169840HectorSanchez@users.noreply.github.com>
2019-02-09 19:20:03 -0700

    Add files via upload

---
+++ Exercise_11/Exercise11.java
@@ -0,0 +1,18 @@
+/* Nombre
+*  Dia: 02/09/2019
+*  Hora: 19:10
+*  Wtd: Write a Java program to print the area and perimeter of a circle.
+*/
+import java.util.Scanner;
+import static java.lang.Math.PI;
+public class Exercise11{
+	public static void main(String args[]){
+	float a;
+	Scanner Sr = new Scanner(System.in);
+	
+		System.out.println("Ingrese el valor de radio: ");
+		a=Sr.nextFloat();
+		System.out.println("Perimetro es ="+(2*PI*a));
+		System.out.println("Area es = "+(PI*a*a));
+}
+}
