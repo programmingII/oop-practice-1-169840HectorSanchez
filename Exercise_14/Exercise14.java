cb81db3618d7e80d1668cc4df399a5e8c97dddca
169840 Hector Sanchez <46726559+169840HectorSanchez@users.noreply.github.com>
2019-02-09 19:50:19 -0700

    Add files via upload

---
+++ Exercise_14/Exercise14.java
@@ -0,0 +1,29 @@
+/* Nombre: Hector_Gerardo_Sanchez_Quiroga
+*  Dia: 02/09/2019
+*  Hora: 19:43
+*  Wtd:Write a Java program to print an American flag on the screen.
+*/
+
+public class Exercise14{
+	public static void main(String args[]){
+
+	System.out.println("* * * * * * ================================== ");
+	System.out.println(" * * * * *  ==================================");
+	System.out.println("* * * * * * ==================================");
+	System.out.println(" * * * * *  ==================================");
+	System.out.println("* * * * * * ==================================");
+	System.out.println(" * * * * *  ==================================");
+	System.out.println("* * * * * * ==================================");
+	System.out.println(" * * * * *  ==================================");
+	System.out.println("* * * * * * ==================================");
+	System.out.println("==============================================");
+	System.out.println("==============================================");
+	System.out.println("==============================================");
+	System.out.println("==============================================");
+	System.out.println("==============================================");
+	System.out.println("==============================================");
+	System.out.println("==============================================");
+
+	
+}
+}
