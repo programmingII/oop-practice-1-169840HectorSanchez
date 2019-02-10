f63fb13c7f68bad398588f5d3f4a41f563f53942
169840 Hector Sanchez <46726559+169840HectorSanchez@users.noreply.github.com>
2019-02-09 20:07:11 -0700

    Add files via upload

---
+++ Exercise_16/Exercise16.java
@@ -0,0 +1,15 @@
+/* Nombre: Hector_Gerardo_Sanchez_Quiroga
+*  Dia: 02/09/2019
+*  Hora: 20:00
+*  Wtd: Write a Java program to print a face
+*/
+public class Exercise16{
+	public static void main(String args[]){
+	System.out.println(" ////////");
+	System.out.println(" |       |");
+	System.out.println("d|  0 0  |b");
+	System.out.println(" |   A   |");
+	System.out.println(" |  ===  |");
+	System.out.println(" +-------+");
+}
+}
