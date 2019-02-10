c08f931074565742b568cb2ff03eb3428f90b193
169840 Hector Sanchez <46726559+169840HectorSanchez@users.noreply.github.com>
2019-02-09 19:33:44 -0700

    Add files via upload

---
+++ Exercise_12/Exercise12.java
@@ -0,0 +1,24 @@
+/* Nombre: Hector_Gerardo_Sanchez_Quiroga
+
+*  Dia: 02/09/2019
+
+*  Hora: 19:28
+
+*  Wtd: Write a Java program that takes three numbers as input to calculate and print the average of the numbers.*/
+
+import java.util.Scanner;
+
+public class Exercise12{
+	public static void main(String args[]){
+		float a,b,c;
+		Scanner Sr = new Scanner(System.in);
+		
+		System.out.println("Ingrese el primer termino: ");
+			a=Sr.nextFloat();
+		System.out.println("Ingrese el segundo termino: ");
+			b=Sr.nextFloat();
+		System.out.println("Ingrese el tercer termino: ");
+			c=Sr.nextFloat();
+		System.out.println("Resultado = "+((a+b+c)/3));
+}
+}
