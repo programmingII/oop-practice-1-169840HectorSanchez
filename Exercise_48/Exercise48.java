d202a1a49cd3ab564a0d8707d382161666df570a
169840 Hector Sanchez <46726559+169840HectorSanchez@users.noreply.github.com>
2019-02-24 20:28:55 -0700

    Add files via upload

---
+++ Exercise_48/Exercise48.java
@@ -0,0 +1,14 @@
+/* Nombre: Hector_Gerardo_Sanchez_Quiroga
+*  Dia: 02/24/2019
+*  Hora: 20:24
+*  Wtd: Write a Java program to print the odd numbers from 1 to 99. Prints one number per line.
+*/
+
+public class Exercise48 {
+  public static void main(String[] args) { 
+	for(int i=1;i<100;i+=2){
+	System.out.println(i+"\n");
+//Solamente imprime los numero impares, por un for
+	}
+  }
+}
