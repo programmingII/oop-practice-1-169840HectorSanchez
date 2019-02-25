c0fa906e8905dd13514786c617e02c16b4d33360
169840 Hector Sanchez <46726559+169840HectorSanchez@users.noreply.github.com>
2019-02-24 18:58:35 -0700

    Add files via upload

---
+++ Exercise_43/Exercise43.java
@@ -0,0 +1,12 @@
+/* Nombre: Hector_Gerardo_Sanchez_Quiroga
+*  Dia: 02/23/2019
+*  Hora: 18:53
+*  Wtd: Write a Java program to print the following string in a specific format *  (see the output).
+*/
+public class Exercise43 {
+  public static void main(String[] args) {
+	System.out.println("Twinkle, twinkle, little star,\nHow I wonder what you are! \nUp above the world so high,\nLike a diamond in the sky. \n Twinkle, twinkle, little star, \nHow I wonder what you are");
+	
+	
+  }
+}
