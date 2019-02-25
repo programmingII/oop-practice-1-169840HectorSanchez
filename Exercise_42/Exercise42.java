3c83fa6d000705e21f1f7a39d8097424c78c5b37
169840 Hector Sanchez <46726559+169840HectorSanchez@users.noreply.github.com>
2019-02-24 19:10:47 -0700

    Update Exercise42.java

--- Exercise_42/Exercise42.java
+++ Exercise_42/Exercise42.java
@@ -1,5 +1,5 @@
 /* Nombre: Hector_Gerardo_Sanchez_Quiroga
-*  Dia: 02/23/2019
+*  Dia: 02/24/2019
 *  Hora: 14:28
 *  Wtd: Write a Java program to input and display your password.
 */
@@ -9,18 +9,18 @@ public class Exercise42 {
 	Scanner Sr = new Scanner(System.in);
 	//Crea objeto scanner 
 		//guardara lo Requerido 
-	System.out.println("Ingresa la contraseña: ");
+	System.out.println("Ingresa la contraseÃ±a: ");
 	String x=Sr.nextLine();
 	
-	System.out.println("Ingresa la contraseña originaria: ");
+	System.out.println("Ingresa la contraseÃ±a originaria: ");
 	String z=Sr.nextLine();
 	if(new String(z).equals(x)){
 /*equals(Object a,Object b)
 Returns true if the arguments are equal to each other and false otherwise.
 ref: https://docs.oracle.com/javase/7/docs/api/java/util/Objects.html#equals(java.lang.Object,%20java.lang.Object)*/
-	System.out.println("La contraseña fue: "+x);
+	System.out.println("La contraseÃ±a fue: "+x);
 }else{
-	System.out.println("Contraseña Incorrecta");
+	System.out.println("ContraseÃ±a Incorrecta");
 }
 	
   }
