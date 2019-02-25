6c3a6fa7865140885b7f95b2c4080e9de598b8c2
169840 Hector Sanchez <46726559+169840HectorSanchez@users.noreply.github.com>
2019-02-25 13:13:50 -0700

    Add files via upload

---
+++ Exercise_59/Exercise59.java
@@ -0,0 +1,18 @@
+/* Nombre: Hector_Gerardo_Sanchez_Quiroga
+*  Dia: 02/25/2019
+*  Hora: 12:20
+*  Wtd: Write a Java program to convert a string to an integer in Java.
+*/
+import java.util.Scanner;
+import java.lang.String;
+/*The String class represents character strings.*/
+public class Exercise59{
+	public static void main(String[] args){
+	Scanner Sr = new Scanner(System.in);
+	//Crea objeto scanner 
+		//guardara lo Requerido 
+	System.out.println("Ingresa la frase: ");
+	String x = Sr.nextLine();
+	System.out.println(x.toLowerCase());
+}
+}
