6f6fe8dec6a1cc631e41fcb5679d9762b4647676
169840 Hector Sanchez <46726559+169840HectorSanchez@users.noreply.github.com>
2019-02-24 19:11:08 -0700

    Update Exercise43.java

--- Exercise_43/Exercise43.java
+++ Exercise_43/Exercise43.java
@@ -1,5 +1,5 @@
 /* Nombre: Hector_Gerardo_Sanchez_Quiroga
-*  Dia: 02/23/2019
+*  Dia: 02/24/2019
 *  Hora: 18:53
 *  Wtd: Write a Java program to print the following string in a specific format *  (see the output).
 */
